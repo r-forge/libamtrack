7b791df5f3ac90b2050fa9fae255dd1e606c8963
sgreilich (Steffen Greilich) <s.greilich@dkfz.de>
2015-05-20 18:49:08 +0000

    Auto sync with main development line at sourceforge
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/libamtrack@295 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/initial.R
+++ pkg/R/initial.R
@@ -2,7 +2,7 @@
  library.dynam("libamtrack", pkg, lib)
 }
 .onAttach <- function(lib, pkg){
- packageStartupMessage("This is libamtrack 0.6.0 'Black Armadillo' (Development, 1335:1340M, 2015-05-20 20:44:10).\nType '?libamtrack' for help.\n")
+ packageStartupMessage("This is libamtrack 0.6.0 'Black Armadillo' (Development, 1335:1340M, 2015-05-20 20:48:18).\nType '?libamtrack' for help.\n")
 }
 .onUnload <- function(libpath){
  try(library.dynam.unload("libamtrack", libpath))
