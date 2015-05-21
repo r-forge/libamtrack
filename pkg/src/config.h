79b80174bfecd5c0b15ce343d73b21dc32aef50f
sgreilich (Steffen Greilich) <s.greilich@dkfz.de>
2015-05-21 21:10:02 +0000

    Auto sync with main development line at sourceforge
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/libamtrack@300 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/config.h
+++ pkg/src/config.h
@@ -2,10 +2,10 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Cryptonym to faciliate versioning for users */
-/* #undef CODE_NAME */
+#define CODE_NAME "Blue Armadillo"
 
 /* Status of the code */
-/* #undef CODE_STATUS */
+#define CODE_STATUS "Release"
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
@@ -85,22 +85,22 @@
 #define PACKAGE "libamtrack"
 
 /* Define to the address where bug reports for this package should be sent. */
-/* #undef PACKAGE_BUGREPORT */
+#define PACKAGE_BUGREPORT "s.greilich@dkfz.de"
 
 /* Define to the full name of this package. */
-/* #undef PACKAGE_NAME */
+#define PACKAGE_NAME "libamtrack"
 
 /* Define to the full name and version of this package. */
-/* #undef PACKAGE_STRING */
+#define PACKAGE_STRING "libamtrack 0.6.1"
 
 /* Define to the one symbol short name of this package. */
-/* #undef PACKAGE_TARNAME */
+#define PACKAGE_TARNAME "libamtrack"
 
 /* Define to the home page for this package. */
-/* #undef PACKAGE_URL */
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-/* #undef PACKAGE_VERSION */
+#define PACKAGE_VERSION "0.6.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
